f2935788a34700c135907da8db6e65ed46085b3e
Michael Specht <micha.specht@gmail.com>
2010-06-02 14:49:49 +0200

    Replaced old 'SimQuant' GPL occurences with 'Proteomic Toolbox'.

--- src/MzDataHandler.cpp
+++ src/MzDataHandler.cpp
@@ -1,20 +1,20 @@
 /*
 Copyright (c) 2007-2008 Michael Specht
 
-This file is part of SimQuant.
+This file is part of Proteomics Toolbox.
 
-SimQuant is free software: you can redistribute it and/or modify
+Proteomics Toolbox is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-SimQuant is distributed in the hope that it will be useful,
+Proteomics Toolbox is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with SimQuant.  If not, see <http://www.gnu.org/licenses/>.
+along with Proteomics Toolbox.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <ptb/MzDataHandler.h>
