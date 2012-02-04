4ca0504e31b34884404551b3e9bf9637df5f7f5c
Michael Specht <micha.specht@gmail.com>
2012-02-04 23:00:08 +0100

    updated bootstrapping script to produce a statis libquazip

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -15,6 +15,7 @@ cd bootstrap
 wget -O quazip.zip "http://downloads.sourceforge.net/project/quazip/quazip/0.4.4/quazip-0.4.4.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fquazip%2F&ts=1328389079&use_mirror=heanet"
 unzip quazip.zip
 cd quazip-0.4.4
+sed -i 's/SHARED/STATIC/g' quazip/CMakeLists.txt
 cmake .
 make
 sudo make install
